e8226cff46fbc0f4987bd3a28f2418375ecf6890
keisuke1223 <keisuke1223shukatsu@gmail.com>
2020-09-06 22:34:02 +0900

    言語対応

---
+++ resources/lang/ja/messages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Login' => 'ログイン',
+    'Logout' => 'ログアウト',
+    'Register' => '登録',
+    'E-Mail Address' => 'メールアドレス',
+    'Password' => 'パスワード',
+    'Remember Me' => 'ログイン情報を記憶する',
+    'Forgot Your Password?' => 'パスワードを忘れた方はこちら',
+];
