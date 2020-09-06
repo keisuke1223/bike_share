a7597329c3e252283426b7aa9c6ed0d391ba72b2
keisuke1223 <keisuke1223shukatsu@gmail.com>
2020-09-06 18:14:15 +0900

    環境構築1

---
+++ resources/views/customer/bike/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.customer')
+
+
+@section('title', 'バイク一覧')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>バイク一覧</h2>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::group(['prefix' => 'customer'], function () {
+    Route::get('bike/index', 'Customer\BikeController@index');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
